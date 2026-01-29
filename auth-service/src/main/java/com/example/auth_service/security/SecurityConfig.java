62304a9a8010b669d24ae6ebf31dc83dd5280346
Ayan Pathan <ayanpathan1500@gmail.com>
2026-01-29 23:45:44 +0530

    Update CORS configuration to allow all origins

--- auth-service/src/main/java/com/example/auth_service/security/SecurityConfig.java
+++ auth-service/src/main/java/com/example/auth_service/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // React app
+        config.setAllowedOriginPatterns(List.of("*")); // React app       <----http://localhost:3000
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
